8d5235c2f2fd4dca843b0d01bc47813da038818c
Pramoda Piyumali <pramodapiyumali@gmail.com>
2022-09-17 22:56:58 +0530

    Add search bar

--- frontend/src/Components/Functions/FinancialManagement/FinancialDetails.js
+++ frontend/src/Components/Functions/FinancialManagement/FinancialDetails.js
@@ -1,5 +1,13 @@
 import React, { Component } from "react";
 import axios from "axios";
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBIcon,
+  MDBCardImage,
+  MDBBtn,
+} from "mdb-react-ui-kit";
 
 export default class FinancialDetails extends Component {
   constructor(props) {
@@ -15,7 +23,7 @@ export default class FinancialDetails extends Component {
   }
 
   retrieveFinancial() {
-    axios.get("http://localhost:8001/financial").then((res) => {
+    axios.get("/financial").then((res) => {
       if (res.data.success) {
         this.setState({
           financial: res.data.existingFinancial,
@@ -27,7 +35,7 @@ export default class FinancialDetails extends Component {
   }
 
   onDelete = (id) => {
-    axios.delete(`http://localhost:8001/financial/delete/${id}`).then((res) => {
+    axios.delete(`/financial/delete/${id}`).then((res) => {
       alert("Delete Successfully");
       this.retrieveFinancial();
     });
@@ -37,9 +45,6 @@ export default class FinancialDetails extends Component {
     const result = financial.filter(
       (financial) =>
         financial.fID.toLowerCase().includes(searchKey) ||
-        financial.totalIncome.toLowerCase().includes(searchKey) ||
-        financial.totalOutcome.toLowerCase().includes(searchKey) ||
-        financial.money.toLowerCase().includes(searchKey) ||
         financial.date.toLowerCase().includes(searchKey) ||
         financial.status.toLowerCase().includes(searchKey)
     );
@@ -50,7 +55,7 @@ export default class FinancialDetails extends Component {
   handleSearchArea = (e) => {
     const searchKey = e.currentTarget.value;
 
-    axios.get("http://localhost:8001/financial").then((res) => {
+    axios.get("/financial").then((res) => {
       if (res.data.success) {
         this.filterData(res.data.existingFinancial, searchKey);
       }
@@ -59,75 +64,83 @@ export default class FinancialDetails extends Component {
 
   render() {
     return (
-      <div className="Khome">
-        <br />
-        <div style={{ width: "20%", marginLeft: "80%" }}>
-          <form className="d-flex">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-              onChange={this.handleSearchArea}
-            ></input>
-          </form>
-        </div>
-
-        <center>
-          <h1>
-            <span class="badge bg-warning text-dark opacity-90 fs-1">
-              All financial details
+      <div className="back fixed" style={{ zIndex: 8 }}>
+        <div className="hc">
+          <br />
+          <div style={{ width: "20%", marginLeft: "80%" }}>
+            <form className="d-flex">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+                onChange={this.handleSearchArea}
+              ></input>
+            </form>
+          </div>
+          <div id="containerJoin">
+            <center>
+              <h1 className="gifJoin">All financial details</h1>
+            </center>
+          </div>
+          <br />
+          <h3>
+            <span class="badge bg-info text-dark opacity-90 ">
+              Financial Information
             </span>
-          </h1>
-        </center>
-
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">FID</th>
-              <th scope="col">Month</th>
-              {/* <th scope="col">Income List</th>
-              <th scope="col">Outcome List</th> */}
-              <th scope="col">Total Income</th>
-              <th scope="col">Total Outcome</th>
-              <th scope="col">Profit or Loss</th>
-              <th scope="col">Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.financial.map((financial, index) => (
-              <tr key={index}>
-                <th scope="row">{index + 1}</th>
-                <td>
-                  <a
-                    href={`/financial/${financial._id}`}
-                    style={{ textDecoration: "none" }}
-                  >
-                    {financial.fId}
-                  </a>
-                </td>
-
-                <td class="table-light">{financial.date}</td>
-                <td class="table-light">{financial.totalIncome}</td>
-                <td class="table-light">{financial.totalOutcome}</td>
-                <td class="table-light">{financial.money}</td>
-
-                <td class="table-light">{financial.status}</td>
+          </h3>
+          <table class="table table-bordered ">
+            <thead class="table-info">
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">FID</th>
+                <th scope="col">Month</th>
+                <th scope="col">Total Income</th>
+                <th scope="col">Total Outcome</th>
+                <th scope="col">Profit or Loss</th>
+                <th scope="col">Status</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {this.state.financial.map((financial, index) => (
+                <tr key={index}>
+                  <th scope="row">{index + 1}</th>
+                  <td class="table-light">
+                    <a
+                      href={`/financial/${financial._id}`}
+                      style={{ textDecoration: "none" }}
+                    >
+                      {financial.fId}
+                    </a>
+                  </td>
 
-        <button className="btn btn-success">
-          <a href="/AddFinancialDetails" style={{ textDecoration: "none", color: "white" }}>
-            Create This Month Financial Statement
-          </a>
-        </button>
+                  <td class="table-light">{financial.date}</td>
+                  <td class="table-light">{financial.totalIncome}</td>
+                  <td class="table-light">{financial.totalOutcome}</td>
+                  <td class="table-light">{financial.money}</td>
 
-        <br></br>
-        <br></br>
-        <br></br>
+                  <td class="table-light">{financial.status}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <br></br>
+          <div>
+            <center>
+              <a
+                className="btn btn-warning text-dark "
+                href="/AddFinancialDetails"
+              >
+                <MDBIcon fas icon="user-plus" />
+                &nbsp;<b>Create This Month Financial Statement</b>
+              </a>
+            </center>
+          </div>
+
+          <br></br>
+          <br></br>
+          <br></br>
+        </div>
       </div>
     );
   }
