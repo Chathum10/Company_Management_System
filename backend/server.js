d3d80410710fbe577d786f03fdca96a87001fb2f
Chathum Adhihetty <89435818+ashen910@users.noreply.github.com>
2022-10-12 19:29:49 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,6 @@ const departmentsRoutes = require("./routes/departments");
 //Financial Management Function Routes
 const financialRoutes = require("./routes/financial");
 
-
 //route middleware
 app.use(employRoutes);
 app.use(projectsRoutes);
