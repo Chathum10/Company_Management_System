44f1330a52a7cad07105bae68def1e65d8960216
Pawani Muthusala Perera <89269937+MuthusalaPerera@users.noreply.github.com>
2022-10-12 20:37:40 +0530

    user update

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose");
 
+
+
 const UserSchema = new mongoose.Schema(
+
+  
   {
+
     name: {
       type: String,
       trim: true,
@@ -18,6 +23,7 @@ const UserSchema = new mongoose.Schema(
     },
     
   },
+
   { timestamps: true }
 );
 
