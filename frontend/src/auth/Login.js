6fe1cd4a4068cab9cd073353a2dfc18814e0d723
Chathum Adhihetty <89167761+Chathum10@users.noreply.github.com>
2022-10-15 18:26:03 +0530

    update

--- frontend/src/auth/Login.js
+++ frontend/src/auth/Login.js
@@ -42,45 +42,50 @@ const Login = (props) => {
 
   return (
     <div className="bdy">
-    <div className="row">
-      <div className="col-sm-1" />
-      <div className="col-sm-4">
-        <br></br>   <br></br> 
-        <h3 className="text-muted text-center mb-5">LOG INTO YOUR ACCOUNT</h3>
-        <div className="card p-5 shadow">
-          <form>
-            <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <input
-                className="form-control"
-                type="email"
-                name="email"
-                value={email}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="password">Password</label>
-              <input
-                className="form-control"
-                type="password"
-                name="password"
-                value={password}
-                onChange={handleChange}
-              />
-            </div>
-            {error ? <p className="text-danger">{error}</p> : null}
-            <div className="text-center">
-              <button className="btn btn-primary" onClick={handleSubmit}>
-                Login
-              </button>
-            </div>
-          </form>
+      <div className="row">
+        <div className="col-sm-1" />
+        <div className="col-sm-4">
+          <br></br>   <br></br>
+          <h3 className="text-muted text-center mb-5">LOG INTO YOUR ACCOUNT</h3>
+          <div className="card p-5 shadow">
+            <form>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input
+                  className="form-control"
+                  type="email"
+                  name="email"
+                  value={email}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input
+                  className="form-control"
+                  type="password"
+                  name="password"
+                  value={password}
+                  onChange={handleChange}
+                />
+              </div>
+              {error ? <p className="text-danger">{error}</p> : null}
+              <div className="text-center">
+                <button className="btn btn-primary" onClick={handleSubmit}>
+                  Login
+                </button>
+              </div><br />
+              <center>
+                <a className="btn btn-text-primary " href="/register">
+                  Don't Have an Account? <b>Register</b>
+                </a>
+              </center>
+            </form>
+          </div>
         </div>
+        <div className="col-sm-2" />
       </div>
-      <div className="col-sm-2" />
-    </div>
-    <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+      <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
     </div>
   );
 };
--- frontend/src/auth/Register.js
+++ frontend/src/auth/Register.js
@@ -36,7 +36,7 @@ const Register = (props) => {
         }
       );
 
-      props.history.push("/login");
+      props.history.push("/");
     } catch (err) {
       setData({ ...data, error: err.response.data.error });
     }
